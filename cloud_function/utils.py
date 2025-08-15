9e74d9a9e8ba09999c86173dcd0b48624677ced3
daniel_grzebyk <daniel.grzebyk@gmail.com>
2025-08-15 20:46:46 +0200

    RNM: move utils to cloud_function/

--- cloud_function/utils.py
+++ cloud_function/utils.py
@@ -10,5 +10,5 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
     blob.upload_from_filename(source_file_name)
 
     print(
-        f"File {source_file_name} uploaded to {destination_blob_name} in bucket {bucket_name}."
+        f"File {source_file_name} uploaded to bucket {bucket_name}."
     )
