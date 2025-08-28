4e536c764b81030201fca6acae6deff7d26bc4d1
daniel_grzebyk <daniel.grzebyk@gmail.com>
2025-08-28 08:59:44 +0200

    ENH: save DataFrame to GCS without saving file locally

--- cloud_function/utils.py
+++ cloud_function/utils.py
@@ -1,12 +1,14 @@
+import logging
+
 from google.cloud import storage
+from pandas import DataFrame
 
 
-def upload_blob(bucket_name: str, source_file_name: str, destination_blob_name: str) -> None:
+def upload_blob(bucket_name: str, df: DataFrame, destination_blob_name: str) -> None:
     """Uploads a file to the bucket."""
     storage_client = storage.Client()
-    bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(destination_blob_name)
-
-    blob.upload_from_filename(source_file_name)
-
-    print(f"File {source_file_name} uploaded to bucket {bucket_name}.")
+    if storage_client.get_bucket(bucket_name).exists():
+        bucket = storage_client.bucket(bucket_name)
+        bucket.blob(destination_blob_name).upload_from_string(df.to_csv(), 'text/csv')
+    else:
+        logging.error("Provided bucket name does not exist.")
